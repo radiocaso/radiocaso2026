27f2aef3101936b24a3d062928115af2a78e38f3
astrosuka <yegresvoselok@gmail.com>
2026-01-17 03:34:20 -0300

    Create Header component with logo and navigation

---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import { useInitialData } from "@/hooks/useInitialData";
+import { urlFor } from "@/lib/sanityImageUrl";
+import NavMenu from "@/components/NavMenu";
+import { NavLink } from "react-router";
+
+export default function Header() {
+  const { data, isLoading, error } = useInitialData();
+
+  if (isLoading) return <div>cargando...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  return (
+    <>
+      <header className="flex justify-between">
+        <NavLink to="/">
+          <div className="flex items-end gap-2">
+            {data?.titulo && (
+              <h1 className="text-2xl font-bold">{data?.titulo}</h1>
+            )}
+            {data?.logo && (
+              <img
+                className="invert"
+                src={
+                  urlFor(data.logo).format("webp").width(50).url() + "&fit=max"
+                }
+              />
+            )}
+          </div>
+        </NavLink>
+        <NavMenu />
+      </header>
+    </>
+  );
+}
