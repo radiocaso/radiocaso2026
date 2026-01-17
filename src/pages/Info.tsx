9e0d1555876b7e22ffbc66075a9919a32073e30e
astrosuka <yegresvoselok@gmail.com>
2026-01-17 03:33:03 -0300

    Create Info page

---
+++ src/pages/Info.tsx
@@ -0,0 +1,15 @@
+import { useInitialData } from "@/hooks/useInitialData";
+import { PortableText } from "@portabletext/react";
+
+export default function Info() {
+  const { data, isLoading, error } = useInitialData();
+
+  if (isLoading) return <div>cargando...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  return (
+    <div className="max-w-prose text-sm">
+      {data?.descripcion && <PortableText value={data.descripcion} />}
+    </div>
+  );
+}
