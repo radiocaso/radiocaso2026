82fe62cb39562dce9b7a66e429ce5a1e340bede0
astrosuka <yegresvoselok@gmail.com>
2026-01-26 13:27:50 -0300

    Add SectionTitle to Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import FutureTransmissions from "@/components/FutureTransmissions";
+import SectionTitle from "@/components/SectionTitle";
 
 export default function Home() {
 
 
   return (
     <>
+      <SectionTitle title="Próximas Transmisiones" />
       <FutureTransmissions />
     </>
   );
