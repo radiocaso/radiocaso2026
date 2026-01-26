8c1f84922c88b6b3d7e94e5c40fcef2f5a28e56b
astrosuka <yegresvoselok@gmail.com>
2026-01-26 13:31:35 -0300

    Load publications data

--- src/pages/Publications.tsx
+++ src/pages/Publications.tsx
@@ -1,8 +1,20 @@
 import SectionTitle from "@/components/SectionTitle";
+import { usePublications } from "@/hooks/usePublications";
+
 export default function Publications() {
+  const { data, isLoading, error } = usePublications();
+
+  if (isLoading) return <div>cargando...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
     <>
       <SectionTitle title="Publicaciones" />
+      <ul>
+        {data?.map((p) => (
+          <li key={p._id}>{p.titulo}</li>
+        ))}
+      </ul>
     </>
   );
 }
